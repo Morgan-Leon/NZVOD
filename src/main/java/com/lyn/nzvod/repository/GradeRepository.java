9f8764e2363fae0947b2d8cd911a8dc05a904633
Morgan.Leon <14127123@bjtu.edu.cn>
2016-10-10 10:17:25 +0800

    Change Env to PC

--- src/main/java/com/lyn/nzvod/repository/GradeRepository.java
+++ src/main/java/com/lyn/nzvod/repository/GradeRepository.java
@@ -2,6 +2,8 @@ package com.lyn.nzvod.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.lyn.nzvod.model.Grade;
+
 public interface GradeRepository extends JpaRepository<Grade, Long> {
 
 }
